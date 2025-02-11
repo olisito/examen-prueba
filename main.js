242b37e9a40c48899b28c3ddd3190630ff1c61dc
David <114335539+olisito@users.noreply.github.com>
2025-02-11 17:25:49 +0100

    ya va

--- main.js
+++ main.js
@@ -6,10 +6,20 @@ const div3 = document.getElementById("div3");
 
 function agregar() {
     const card = document.createElement("div");
+    card.setAttribute("class", "contacto");
     const newp = document.createElement("p");
-    newp.innerHTML = inputnombrevalue;
+    newp.innerHTML = inputnombre.value;
     card.appendChild(newp);
     div3.appendChild(card);
+
+    const telefono = document.createElement("p");
+telefono.innerHTML = inputnumero.value;
+card.appendChild(telefono);
+
+const correo = document.createElement("p");
+correo.innerHTML = inputemail.value;
+card.appendChild(correo);
+
 
     let inputnombrevalue = inputnombre.value;
     let inputnumerovalue = inputnumero.value;
